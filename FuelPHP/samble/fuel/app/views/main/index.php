16b272fb985759b8464c7965000a1752fb404141
kanbaru-github <usupfv@gmail.com>
2023-01-29 23:20:35 +0900

    add JavaScript memo

--- FuelPHP/samble/fuel/app/views/main/index.php
+++ FuelPHP/samble/fuel/app/views/main/index.php
@@ -1,10 +1,12 @@
 <?= Fuel::VERSION ?><br>
 <?= phpversion() ?>
 
-<div id="switch">
-	<p id="sort">sort</p>
-	<p id="delete">delete</p>
-</div>
-
-<div id="current"></div></div>
-<div id="reload">リロード</div>
+<main id="content_type">
+	<div id="switch">
+		<p id="sort">sort</p>
+		<p id="delete">delete</p>
+	</div>
+	
+	<div id="current"></div></div>
+	<div id="reload">リロード</div>
+</main>
--- FuelPHP/samble/public/assets/js/main.js
+++ FuelPHP/samble/public/assets/js/main.js
@@ -1,18 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
-	document.getElementById('switch').addEventListener('click', (e) => {
-		if (e.target = 'p#sort') {
-			console.log('sort');
-			history.pushState({}, '', "#sort");
-			return false;
-		} else if (e.target = 'p#delete') {
-			console.log('delete');
-			history.pushState({}, '', "#delete");
-		}
+	// document.getElementById('switch').addEventListener('click', (e) => {
+	// 	if (e.target = 'p#sort') {
+	// 		console.log('sort');
+	// 		history.pushState({}, '', "#sort");
+	// 		return false;
+	// 	} else if (e.target = 'p#delete') {
+	// 		console.log('delete');
+	// 		history.pushState({}, '', "#delete");
+	// 	}
 
 		if (location.href.match(/#sort/)) {
 			document.getElementById('current').textContent = 'sort';
 		}
+	// });
+
+	const contentType = document.getElementById('content_type');
+	document.getElementById('sort').addEventListener('click', () => {
+		// ページを読み込まずにでURLを変更
+		history.pushState({}, '', "#sort");
+		contentType.classList.remove('delete');
 	});
+	document.getElementById('delete').addEventListener('click', () => {
+		history.pushState({}, '', "#delete");
+		contentType.classList.add('delete');
+	});
+
+	if (location.href.match(/#delete/)) {
+
+	}
+
 	document.getElementById('reload').addEventListener('click', () => {
 		location.reload();
 	})
